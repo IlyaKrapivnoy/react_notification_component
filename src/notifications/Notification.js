41deebfe7bf326caebdc41a8fe2e6f1ac9f60d4d
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-20 20:16:52 +0300

    mapping data to the component

---
+++ src/notifications/Notification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notification = (props) => {
+    return (
+        <div className='notification-item'>
+            <p>{props.message}</p>
+            <div className='bar'></div>;
+        </div>
+    );
+};
+
+export default Notification;
--- src/notifications/NotificationProvider.js
+++ src/notifications/NotificationProvider.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { v4 } from 'uuid';
+import Notification from './Notification';
 
 const NotificationProvider = (props) => {
     const notifications = [
         {
             id: v4(),
+            type: 'SUCCESS',
+            message: 'Hell yeah!',
         },
     ];
     console.log(notifications);
 
-    return <div>{props.children}</div>;
+    return (
+        <div>
+            <div className='notification-wrapper'>
+                {notifications.map((note) => {
+                    return <Notification {...note} />;
+                })}
+            </div>
+            {props.children}
+        </div>
+    );
 };
 
 export default NotificationProvider;
