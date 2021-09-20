cf5be48d9e43d9ae8f6521c5a2718fb3f51bc69e
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-20 20:26:41 +0300

    add dynamic styles

--- src/notifications/Notification.js
+++ src/notifications/Notification.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Notification = (props) => {
     return (
-        <div className='notification-item'>
+        <div
+            className={`notification-item ${
+                props.type === 'SUCCESS' ? 'success' : 'error'
+            } `}
+        >
             <p>{props.message}</p>
             <div className='bar'></div>
         </div>
