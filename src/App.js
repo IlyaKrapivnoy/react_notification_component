85683405ed4ecc6c87199dfaf3abd8030932a1d5
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-21 10:58:30 +0300

    dispatch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { useContext } from 'react';
 import './App.css';
 import { NotificationContext } from './notifications/NotificationProvider';
+import { v4 } from 'uuid';
 
 function App() {
-    const contextValue = useContext(NotificationContext);
-    console.log('contextValue', contextValue);
+    const dispatch = useContext(NotificationContext);
+
+    dispatch({
+        type: 'ADD_NOTIFICATION',
+        payload: {
+            id: v4(),
+            type: 'SUCCESS',
+            message: 'new notification!',
+        },
+    });
+
+    console.log('contextValue', dispatch);
     return <div className=''></div>;
 }
 
