e41202e9a4a387780e819125d6c93bd21d00e293
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-21 10:59:44 +0300

    change to useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import './App.css';
 import { NotificationContext } from './notifications/NotificationProvider';
 import { v4 } from 'uuid';
@@ -6,13 +6,15 @@ import { v4 } from 'uuid';
 function App() {
     const dispatch = useContext(NotificationContext);
 
-    dispatch({
-        type: 'ADD_NOTIFICATION',
-        payload: {
-            id: v4(),
-            type: 'SUCCESS',
-            message: 'new notification!',
-        },
+    useEffect(() => {
+        dispatch({
+            type: 'ADD_NOTIFICATION',
+            payload: {
+                id: v4(),
+                type: 'SUCCESS',
+                message: 'Hello, I am a new notification!',
+            },
+        });
     });
 
     console.log('contextValue', dispatch);
